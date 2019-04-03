2a0e1268b378c3c44d5f145c73453d47d8b78cad
neopoole <carracherk@gmail.com>
2019-04-03 23:18:12 +0100

    associate users with campgrounds and with comments

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,11 +14,15 @@ router.get("/", function(req, res){
 });
 
 //Create - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
@@ -29,7 +33,7 @@ router.post("/", function(req, res){
 });
 
 //New - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -43,5 +47,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+//Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
